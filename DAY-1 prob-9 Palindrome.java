4dabb4aeb07d4b82efba4796483ae37775a89b12
yuvageetha2007-droid <yuvageetha2007@gmail.com>
2026-07-14 11:33:54 +0530

    Create DAY-1 prob-9 Palindrome.java

---
+++ DAY-1 prob-9 Palindrome.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        int original=x;
+        int reversed=0;
+        while(x!=0){
+            int digit=x%10;
+            reversed=reversed*10+digit;
+            x/=10;
+        }
+        return original==reversed;
+        }
+        }
