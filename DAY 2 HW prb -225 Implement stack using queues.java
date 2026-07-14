5c34b5dbd59c5cd73256910dde16c33a1c00a43d
yuvageetha2007-droid <yuvageetha2007@gmail.com>
2026-07-14 21:22:52 +0530

    Create DAY 2 HW prb -225 Implement stack using queues.java

---
+++ DAY 2 HW prb -225 Implement stack using queues.java
@@ -0,0 +1,38 @@
+ import java.util.Queue;
+import java.util.LinkedList;
+
+class MyStack {
+
+    Queue<Integer> q1;
+    Queue<Integer> q2;
+
+    public MyStack() {
+        q1 = new LinkedList<>();
+        q2 = new LinkedList<>();
+    }
+
+    public void push(int x) {
+        q2.offer(x);
+
+        while (!q1.isEmpty()) {
+            q2.offer(q1.poll());
+        }
+
+        Queue<Integer> temp = q1;
+        q1 = q2;
+        q2 = temp;
+    }
+
+    public int pop() {
+        return q1.poll();
+    }
+
+    public int top() {
+        return q1.peek();
+    }
+
+    public boolean empty() {
+        return q1.isEmpty();
+    }
+}
+
