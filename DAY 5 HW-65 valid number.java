8e39c09b014e1bc3bc7e50d4705764a83764d052
yuvageetha2007-droid <yuvageetha2007@gmail.com>
2026-07-17 20:51:05 +0530

    Create DAY 5 HW-65 valid number.java

---
+++ DAY 5 HW-65 valid number.java
@@ -0,0 +1,36 @@
+ class Solution {
+    public boolean isNumber(String s) {
+        s = s.trim();
+
+        boolean num = false;
+        boolean exp = false;
+        boolean dot = false;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (Character.isDigit(c)) {
+                num = true;
+            } 
+            else if (c == '.') {
+                if (dot || exp) return false;
+                dot = true;
+            } 
+            else if (c == 'e' || c == 'E') {
+                if (exp || !num) return false;
+                exp = true;
+                num = false;
+            } 
+            else if (c == '+' || c == '-') {
+                if (i != 0 && s.charAt(i - 1) != 'e' && s.charAt(i - 1) != 'E')
+                    return false;
+            } 
+            else {
+                return false;
+            }
+        }
+
+        return num;
+    }
+}
+ 
