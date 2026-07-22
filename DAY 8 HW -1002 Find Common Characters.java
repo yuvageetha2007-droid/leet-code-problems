8409af2482cf1d04d85d6c2745d3f507e467db69
yuvageetha2007-droid <yuvageetha2007@gmail.com>
2026-07-22 13:13:57 +0530

    Create DAY 8 HW -1002 Find Common Characters.java

---
+++ DAY 8 HW -1002 Find Common Characters.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    public List<String> commonChars(String[] words) {
+
+        int[] minFreq = new int[26];
+        Arrays.fill(minFreq, Integer.MAX_VALUE);
+
+        for (String word : words) {
+
+            int[] freq = new int[26];
+
+            for (char ch : word.toCharArray()) {
+                freq[ch - 'a']++;
+            }
+
+            for (int i = 0; i < 26; i++) {
+                minFreq[i] = Math.min(minFreq[i], freq[i]);
+            }
+        }
+
+        List<String> result = new ArrayList<>();
+
+        for (int i = 0; i < 26; i++) {
+            while (minFreq[i] > 0) {
+                result.add(String.valueOf((char) (i + 'a')));
+                minFreq[i]--;
+            }
+        }
+
+        return result;
+    }
+}
